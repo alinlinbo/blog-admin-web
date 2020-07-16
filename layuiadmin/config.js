dcaa81bf05090edec5d4bea6b7fa394b7901a9c0
alinlinbo <1144381579@qq.com>
2020-07-17 03:16:43 +0800

    表单验证，菜单加载

--- layuiadmin/config.js
+++ layuiadmin/config.js
@@ -15,7 +15,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
     ,entry: 'index' //默认视图文件名
     ,engine: '.html' //视图文件后缀名
     ,pageTabs: true //是否开启页面选项卡功能。iframe版推荐开启
-    ,baseUrl:'http://192.168.0.107:3000/'
+    ,baseUrl:'http://139.9.94.141:3000/'
 
     ,name: 'layuiAdmin'
     ,tableName: 'layuiAdmin' //本地存储表名
--- layuiadmin/modules/ajax.js
+++ layuiadmin/modules/ajax.js
@@ -1,33 +1,71 @@
-layui.define(['index'],function (exports) {
-    const req = function (params) {
-        params.url = layui.setter.baseUrl+params.url
-        params.data = JSON.stringify(params.data)
-        if (!params.contentType){ // 没有提供默认的contentType，就给一个默认值
-            params.contentType = 'application/json'
-        }
-        params.header = {
+layui.define(['index','form'],function (exports) {
+    layui.$.ajaxSetup({
+        headers : {
             Authorization: "Bearer "+localStorage.getItem('token')
-        }
-        if (params.success){
-            const originSuc = params.success
-            params.success = function (res) {
-                if (res.code===0){
-                    originSuc(res)
-                }else {
-                    layer.msg(res.msg)
+        },
+        beforeSend : function(xhr, params) { // 发送请求前触发
+            params.url = layui.setter.baseUrl+params.url
+            if (!params.contentType){
+                params.contentType = 'application/json' //参数格式指定application/jaon
+            }
+            if (params.contentType==='application/json'||params.contentType==='json'){
+                params.data = JSON.stringify(params.data)   //JSON.stringify()将它转化为json字符串
+            }
+            if (params.success){
+                const  originSuc = params.success
+                params.success = function (res) {
+                    if (res.code===0){
+                        originSuc(res)
+                    }else{
+                        layer.msg(res.msg)
+                    }
                 }
             }
-        }
-        if (!params.error){  /// 没有提供error处理方法，就给一个默认的操作
+            const originError = params.error
             params.error = function (res) {
                 console.log('错误信息',res)
-                // layer.msg('错误码:'+res.status+',错误信息:'+res.statusText)
-                layer.msg(`错误码:${res.status},错误信息:${res.statusText}`)
+                if (res.status === 401){
+                    layer.msg('登录验证已过期，即将跳转到登录页面', {
+                        time: 1000
+                    },function(){
+                        window.parent.parent.location.href =window.origin+ "/blog-admin-web/views/user/login.html"
+                    });
+                } else {
+                    layer.msg('错误码：'+res.status+"，错误信息："+res.statusText)
+                }
+                originError(res)
             }
-        }
-        layui.$.ajax(params)
-    }
+        },
+    });
 
-    //输出test接口
-    exports('req', req);
+    var form = layui.form
+    form.verify({
+        password: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+        ],
+        compareBothPwd:function (value, item) {
+            const password = $("input[name='password']").val(),
+                repassword = $("input[name='repassword']").val();
+            if (password!==repassword){
+                return '前后密码不一致'
+            }
+        },
+        username: function(usernameV){ //value：表单的值、item：表单的DOM对象
+            if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(usernameV)){
+                return '用户名不能有特殊字符';
+            }
+            if(/(^\_)|(\__)|(\_+$)/.test(usernameV)){
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if(/^\d+\d+\d$/.test(usernameV)){
+                return '用户名不能全为数字';
+            }
+            // if(/[\S]{6,12}$/.test(usernameV)){
+            //     return '用户名必须6到12位，且不能出现空格';
+            // }
+        },
+    });
+    exports('req', {});
+
 })
